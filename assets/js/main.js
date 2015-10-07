71dbb21a05ca5a685c86dc161c817d23d477ef3c
Aelia Abbas <aelia.abbas@cityrdbclab5015.auckland.natcoll.ac.nz>
2015-10-07 15:45:38 +1300

    first

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// Hides those paragaphs when clicked
+
+$(document).ready(function(){
+	$("p").click(function(){
+		$(this).hide();
+	});
+});
+
+// $(document).ready(function(){
+// 	$("p").click(function(){
+// 		$(this).show();
+// 	});
+// });
